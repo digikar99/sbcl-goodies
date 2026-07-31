e4207ae2489049d854d9e467c6949b6f5e8fea2b
digikar99 <digikar@proton.me>
2026-07-31 21:18:50 +0200

    Modularize build into build-linux and build-release-all

--- scripts/upload_release.sh
+++ scripts/upload_release.sh
@@ -3,33 +3,24 @@
 source $(dirname ${0})/lib.sh
 
 SBCL_VERSION=${1}
-ASDF_VERSION=${2}
-REVISION=${3}
-LIBFIXPOSIX_VERSION=${4}
-OPENSSL_VERSION=${5}
-LIBTLS_VERSION=${6}
+REVISION=${2}
 
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-git config user.name "Stelian Ionescu"
-git config user.email "sionescu@cddr.org"
+git config user.name "Shubhamkar Ayare"
+git config user.email "digikar@proton.me"
 
 RELEASE="${SBCL_VERSION}+r${REVISION}"
 TAG="v${RELEASE}"
-git tag -m "Release ${RELEASE}" ${TAG}
-git push --tags
 
-cat > notes.md << EOF
-# Components:
- - SBCL ${SBCL_VERSION}
- - ASDF ${ASDF_VERSION}
- - libfixposix ${LIBFIXPOSIX_VERSION}
- - OpenSSL ${OPENSSL_VERSION}
- - LibTLS ${LIBTLS_VERSION}
-EOF
+touch notes.md
+cat tarballs/linux-notes.md >> notes.md
+cat tarballs/macos-notes.md >> notes.md
+cat tarballs/windows-notes.md >> notes.md
+
 gh release create \
    ${TAG} \
    --latest \
    --title "SBCL ${RELEASE}" \
    --notes-file notes.md \
-   "sbcl-${RELEASE}-source.tar.bz2" \
-   "sbcl-${RELEASE}-x86-64-linux-binary.tar.bz2"
+   tarballs/sbcl-${RELEASE}-$(uname -m)-{linux,darwin,windows}-source.tar.bz2 \
+   tarballs/sbcl-${RELEASE}-$(uname -m)-{linux,darwin,windows}-binary.tar.bz2
